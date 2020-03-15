0f145bc501cb7a7ae3732af26d180f9d1cdd85a6
Rodrigo de Almeida Pereira <rodrigodealmeidapereira@Rodrigos-MacBook-Pro.local>
2020-03-15 11:08:31 -0300

    Porject init

---
+++ batchregistration/src/main/java/br/com/fiap/billing/batchregistration/BatchregistrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.billing.batchregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchregistrationApplication.class, args);
+	}
+
+}
---
+++ batchregistration/src/test/java/br/com/fiap/billing/batchregistration/BatchregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.billing.batchregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
